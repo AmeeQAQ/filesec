44606538187ca93a4029550f49751f1b1f179a0a
AmeeeQAQ <pablo.ortegah@um.es>
2023-03-30 01:13:48 +0200

    added support for password changes

--- filesec.py
+++ filesec.py
@@ -13,88 +13,158 @@ def keygen(passwd):
     salt = os.urandom(16)
     kdf = PBKDF2HMAC(hashes.SHA256(),32,salt,480000)
     # k2 derived from password to encrypt k1
-    k2 = kdf.derive(passwd.encode())
+    k2 = kdf.derive(passwd)
     # Initialization Vector for AES256
     iv = os.urandom(16)
     # Encrypting k1
-    cipher = Cipher(algorithms.AES256(k2), modes.CBC(iv))
-    k1_encrypted = cipher.encryptor().update(k1) + cipher.encryptor().finalize()
+    cryptguard = Cipher(algorithms.AES(k2), modes.CBC(iv))
+    k1_encrypted = cryptguard.encryptor().update(k1) + cryptguard.encryptor().finalize()
     # Hash k2 for storing
     digest = hashes.Hash(hashes.SHA256())
-    k2_hashed = digest.update(k2) + digest.finalize()
+    digest.update(k2) 
+    k2_hashed = digest.finalize()
     # Generate plaintext file for storage
     f = open('keys.txt', 'w')
     f.write(k1_encrypted.hex() + '::' + iv.hex() + '::' + k2_hashed.hex() + '::' + salt.hex())
 
+# Authentication function to check if password is correct
+def auth(passwd, keys):
+    kdf = PBKDF2HMAC(hashes.SHA256(),32,bytes.fromhex(keys[3]),480000)
+    k2auth = kdf.derive(passwd)
+    digest = hashes.Hash(hashes.SHA256())
+    digest.update(k2auth)
+    hashkey = digest.finalize()
+    return hashkey.hex() == keys[2]
+
+# Decryption function for k1
+def dk1(passwd, salt, ck1, iv):
+    kdf = PBKDF2HMAC(hashes.SHA256(),32,salt,480000)
+    k2 = kdf.derive(passwd)
+    cryptguard = Cipher(algorithms.AES(k2),modes.CBC(iv))
+    k1 = cryptguard.decryptor().update(ck1) + cryptguard.decryptor().finalize()
+    return k1
+
+# The (en)Crypt
 def filecrypt(file, passwd):
     # Check if file exist
     if os.path.isfile(file):
-        # Read contents of file
-        f = open(file, "rb")
-        fread = f.read()
+        # Check passwd against hashed k2
+        f = open('keys.txt', 'r')
+        keyring = f.read()
         f.close()
-        # Initialization Vector for encryption
-        iv = os.urandom(16)
-        # Cryptguard Cipher
-        cryptguard = Cipher(algorithms.AES(key), modes.CBC(iv))
-        # Padder
-        padder = padding.PKCS7(128).padder()
-        padded_content = padder.update(fread) + padder.finalize()
-        # Into the crypt
-        tomb = cryptguard.encryptor().update(padded_content) + cryptguard.encryptor().finalize()
-        f = open(file, 'wb')
-        f.write(tomb)
-        f.close()
-        # Write tombstone
-        f = open('tomb.txt', 'w')
-        f.write(file + '::' + iv.hex())
+        keys = keyring.split('::')
+        if auth(passwd, keys):
+            # Decrypt k1
+            k1 = dk1(passwd, bytes.fromhex(keys[3]), bytes.fromhex(keys[0]), bytes.fromhex(keys[1]))
+            # Read contents of file
+            f = open(file, "rb")
+            fread = f.read()
+            f.close()
+            # Initialization Vector for encryption
+            iv = os.urandom(16)
+            # Call the Cryptguard
+            cryptguard = Cipher(algorithms.AES(k1), modes.CBC(iv))
+            # Padder
+            padder = padding.PKCS7(128).padder()
+            bury = padder.update(fread) + padder.finalize()
+            # Into the Crypt
+            tomb = cryptguard.encryptor().update(bury) + cryptguard.encryptor().finalize()
+            f = open(file, 'wb')
+            f.write(tomb)
+            f.close()
+            # Write tombstone
+            f = open('tomb.txt', 'w')
+            f.write(file + '::' + iv.hex())
+        else:
+            print("Password mismatch")
     else:
-        print("file not found")
+        print("File not found")
 
-def decryption(file, passwd):
-    f = open(file, "rb")
-    fread = f.read()
-    f.close()
-    f = open('passwd.txt', 'r')
-    secrets = f.read()
-    entry = secrets.split('::')
-    if entry[0] == file:
-        kdf = PBKDF2HMAC (hashes.SHA256(),32,bytes.fromhex(entry[2]),480000)
-        try:
-            kdf.verify(passwd.encode(), bytes.fromhex(entry[1]))
-        except cryptography.exceptions.InvalidKey:
-            print('The entered password is not correct')
-            raise
-        cipher = Cipher(algorithms.AES(bytes.fromhex(entry[1])), modes.CBC(bytes.fromhex(entry[3])))
-        decryptor = cipher.decryptor()
-        unpadder = padding.PKCS7(128).unpadder()
-        deciphered_content = decryptor.update(fread) + decryptor.finalize()
-        unpadded_file = unpadder.update(deciphered_content) + unpadder.finalize()
-        f = open(file, 'wb')
-        f.write(unpadded_file)
+# The (de)Crypt
+def filedecrypt(file, passwd):
+    # Check if file exist
+    if os.path.isfile(file):
+        f = open('keys.txt', 'r')
+        keyring = f.read()
         f.close()
+        keys = keyring.split('::')
+        # Password auth
+        if auth(passwd, keys):
+            # Decrypt k1
+            k1 = dk1(passwd, bytes.fromhex(keys[3]), bytes.fromhex(keys[0]), bytes.fromhex(keys[1]))
+            # Read tombstone
+            f = open('tomb.txt', 'r')
+            inscription = f.read()
+            f.close()
+            filecrypted = inscription.split('::')
+            # Call the Cryptguard
+            cryptguard = Cipher(algorithms.AES(k1), modes.CBC(bytes.fromhex(filecrypted[1])))
+            unpadder = padding.PKCS7(128).unpadder()
+            f = open(file, 'rb')
+            corpse = f.read()
+            f.close()
+            unearthed = cryptguard.decryptor().update(corpse) + cryptguard.decryptor().finalize()
+            revived = unpadder.update(unearthed) + unpadder.finalize()
+            f = open(file, 'wb')
+            f.write(revived)
+            f.close()
+        else:
+            print('Password mismatch')
+    else:
+        print('File not found')
+
+def changepass(newpasswd, k1):
+    # Salt is the spice of life
+    salt = os.urandom(16)
+    kdf = PBKDF2HMAC(hashes.SHA256(),32,salt,480000)
+    # k2 derived from password to encrypt k1
+    k2 = kdf.derive(newpasswd)
+    # Initialization Vector for AES
+    iv = os.urandom(16)
+    # Encrypting k1
+    cipher = Cipher(algorithms.AES(k2), modes.CBC(iv))
+    k1_encrypted = cipher.encryptor().update(k1) + cipher.encryptor().finalize()
+    # Hash k2 for storing
+    digest = hashes.Hash(hashes.SHA256())
+    digest.update(k2) 
+    k2_hashed = digest.finalize()
+    # Generate plaintext file for storage
+    f = open('keys.txt', 'w')
+    f.write(k1_encrypted.hex() + '::' + iv.hex() + '::' + k2_hashed.hex() + '::' + salt.hex())
     
 def parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", 
-                        help="file to be encrypted. Use the absolute route if the file is not in the same directory as the script", required=True)
     parser.add_argument("-m", "--mode", 
-                        help="0 to encrypt, 1 to decrypt", required=True, type=int)
+                        help="0 to encrypt, 1 to decrypt, 2 to establish a new password", required=False, type=int)
+    parser.add_argument("-f", "--file", 
+                        help="file to be encrypted. Use the absolute route if the file is not in the same directory as the script", required=False)
     return parser
 
 def main():
     args = parser().parse_args()
-    if (args.mode == 0):
-        passwd = input('Enter your encrypting password: ')
-        if passwd:
-            filecrypt(args.file, passwd)
-        else:
-            print('Enter a valid password')
-    elif (args.mode == 1):
-        passwd = input('Enter your file password: ')
-        decryption(args.file, passwd)
-    else:
-        print("Mode not supported")
+    passwd = input('Enter encrypt password: ')
+    match args.mode:
+        case 0:
+            filecrypt(args.file, passwd.encode())
+
+        case 1:
+            filedecrypt(args.file, passwd.encode())
+
+        case 2:
+            f = open('keys.txt', 'r')
+            keyring = f.read()
+            f.close()
+            keys = keyring.split('::')
+            if auth(passwd.encode(), keys):
+                newpasswd = input('Enter a new password: ')
+                newconf = input ('Repeat new password: ')
+                if newpasswd == newconf:
+                    k1 = dk1(passwd.encode(), bytes.fromhex(keys[3]), bytes.fromhex(keys[0]), bytes.fromhex(keys[1]))
+                    changepass(newpasswd.encode(), k1)
+                else:
+                    print("Incorrect password")
+            else:
+                print('Password mismatch')
     
 
 if __name__ == '__main__' : 
